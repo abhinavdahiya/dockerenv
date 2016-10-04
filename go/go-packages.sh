dffa04945fd0234ba15f5e9c7beefdc85350381a
Abhinav Dahiya <abhinavdtu2012@gmail.com>
2016-10-03 22:52:43 -0700

    move to alpine image

--- go/go-packages.sh
+++ go/go-packages.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 ## install go packages required
 
-curl -vsSL "https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/protoc-$PROTOBUF_VERSION-linux-x86_64.zip" -o proto.zip \
-  && unzip proto.zip bin/protoc \
-  && mv bin/protoc /usr/local/bin \
-  && rm -rf proto.zip bin \
-  && go get -u -v \
+apk add --no-cache protobuf && rm -rf /var/cache/apk/*
+
+go get -u \
   github.com/jteeuwen/go-bindata/... \
   github.com/elazarl/go-bindata-assetfs/... \
-  github.com/nsf/gocode \
-  golang.org/x/tools/cmd/goimports \
-  github.com/mitchellh/gox \
   github.com/golang/protobuf/proto \
   github.com/golang/protobuf/protoc-gen-go \
   google.golang.org/grpc \
   github.com/gengo/grpc-gateway/protoc-gen-grpc-gateway \
   github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger \
-  github.com/tools/godep \
   github.com/emirpasic/gods/...
